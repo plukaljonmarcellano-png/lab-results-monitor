cc59f250e98903e241c8ea68b096a080ec78a671
plukaljonmarcellano-png <plukaljonmarcellano@gmail.com>
2026-09-01 16:20:28 +0300

    Chart tooltips, test search, and the .seg collision that squashed zone bars

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline shell for Lab Results Monitor. Data always comes from Supabase over the network. */
-var CACHE = "labmon-v18-scroll";
+var CACHE = "labmon-v19-search";
 var SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
