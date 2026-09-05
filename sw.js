9d9829954dd5bbd854dd33aa7e2d0e83feb501b7
plukaljonmarcellano-png <plukaljonmarcellano@gmail.com>
2026-09-05 21:32:48 +0300

    Iron studies: serum iron, TIBC and transferrin saturation

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline shell for Lab Results Monitor. Data always comes from Supabase over the network. */
-var CACHE = "labmon-v23-meds";
+var CACHE = "labmon-v24-iron";
 var SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
