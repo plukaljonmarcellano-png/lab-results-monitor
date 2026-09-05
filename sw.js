71c423d2e8e8dbcef2961c30131a40e28c6b11c6
plukaljonmarcellano-png <plukaljonmarcellano@gmail.com>
2026-09-05 21:37:53 +0300

    save() no longer writes settings columns this session never changed

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Offline shell for Lab Results Monitor. Data always comes from Supabase over the network. */
-var CACHE = "labmon-v24-iron";
+var CACHE = "labmon-v25-settings";
 var SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(e){
